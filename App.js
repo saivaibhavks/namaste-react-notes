d2ba6a1aaaf728a09a53bfce39f4258c37802559
K S Saivaibhav <saivaibhavks2022@gmail.com>
2024-07-31 16:23:29 +0530

    installed parcel

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
 const heading = React.createElement("h1", {}, "Hello from React");
+
+const child1 = React.createElement("h2", { id: "child1" }, "child 1");
+const child2 = React.createElement("h2", {}, "child 2");
+const parent = React.createElement("h1", {}, [child1, child2]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
